384eb23eda184503ca77b6109caaf5b4ce452e0b
SuthattaLindstrii <psuthatta@gmail.com>
2020-09-08 23:47:37 +0200

    Updates

---
+++ precache-manifest.04e006cb98e8059b9ba4993f2e5a2d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bceab966cfd44991ac5f0e48fcd3a2a",
+    "url": "/suthatta/index.html"
+  },
+  {
+    "revision": "41fd3f79ed7c459d3361",
+    "url": "/suthatta/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "988ea7387c9897ce7148",
+    "url": "/suthatta/static/js/2.6fc7c564.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/suthatta/static/js/2.6fc7c564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41fd3f79ed7c459d3361",
+    "url": "/suthatta/static/js/main.d8bb20ca.chunk.js"
+  },
+  {
+    "revision": "e71826a291b6b40d2211",
+    "url": "/suthatta/static/js/runtime-main.af4ae5dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/suthatta/static/media/logo.5d5d9eef.svg"
+  }
+]);
